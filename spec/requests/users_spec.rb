8ff133375dd26a80896d64e81cd2395162e17cc3
takuya kinoshita <kktak02@gmail.com>
2022-11-04 08:51:56 +0900

    mod users_spec

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,25 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
+
+  let(:user) { FactoryBot.create(:user) }
+
   describe "GET /index" do
     it "returns http success" do
-      get "/"
+      get root_path
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET /new" do
     it "returns http success" do
-      get "/users/new"
+      get new_user_path
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET /edit" do
-
+    it 'returns http success' do
+      get edit_user_path(user)
+      expect(response).to have_http_status(:success)
+    end
   end
 
   describe "GET /show" do
+    it 'returns http success' do
+      get user_path(user)
+      expect(response).to have_http_status(:success)
+    end
 
   end
 
